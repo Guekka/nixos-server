84ce9107656eda447231b978a513da9d406327c3
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-02-20 11:47:31 +0100

    required filesystems changes

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -10,6 +10,16 @@
       ./hardware-configuration.nix
     ];
 
+  # filesystems
+  fileSystems."/".options = ["compress=zstd" "noatime" ];
+  fileSystems."/home".options = ["compress=zstd" "noatime" ];
+  fileSystems."/nix".options = ["compress=zstd" "noatime" ];
+  fileSystems."/persist".options = ["compress=zstd" "noatime" ];
+  fileSystems."/persist".neededForBoot = true;
+  
+  fileSystems."/var/log".options = ["compress=zstd" "noatime" ];
+  fileSystems."/var/log".neededForBoot = true;
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
