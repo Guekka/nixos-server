0bcec762bbfb30784c57c693bf2275e3e291d0be
G'k <39066502+Guekka@users.noreply.github.com>
2023-04-17 19:20:41 +0200

    sops: init

--- flake.nix
+++ flake.nix
@@ -2,6 +2,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     impermanence.url = "github:nix-community/impermanence";
+    sops-nix = {
+      url = "github:mic92/sops-nix";
+      inputs.nixpkgs.follows ="nixpkgs";
+      inputs.nixpkgs-stable.follows ="nixpkgs";
+    };
   };
 
   # what will be produced (i.e. the build)
---
+++ modules/sops.nix
@@ -0,0 +1,15 @@
+{ sops-nix, lib, config, ... }:
+let
+  isEd25519 = k: k.type == "ed25519";
+  getKeyPath = k: k.path;
+  keys = builtins.filter isEd25519 config.services.openssh.hostKeys;
+in
+{
+  imports = [
+    sops-nix.nixosModules.sops
+  ];
+
+  sops = {
+    age.sshKeyPaths = map getKeyPath keys;
+  };
+}
