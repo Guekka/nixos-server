308c93b7373d7549001bf16f99989d4881b8cdae
G'k <39066502+Guekka@users.noreply.github.com>
2023-04-17 22:35:36 +0200

    file-server: init

---
+++ hosts/horus/web-server.nix
@@ -0,0 +1,11 @@
+{
+    services.nginx.virtualHosts."e1.oze.li" = {
+      addSSL = true;
+      enableACME = true;
+      root = "/var/www/files";
+    };
+
+    environment.persistence = {
+      "/persist".directories = [ "/var/www/files" ];
+    };
+}
