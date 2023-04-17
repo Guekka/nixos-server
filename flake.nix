246b0e297554531fd54684313a8c4749c63b20d7
G'k <39066502+Guekka@users.noreply.github.com>
2023-04-17 19:14:01 +0200

    refactor: move to flakes

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    impermanence.url = "github:nix-community/impermanence";
+  };
+
+  # what will be produced (i.e. the build)
+  outputs = { nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      server = nixpkgs.lib.nixosSystem {
+        packages = nixpkgs.legacyPackages.x86_64-linux;
+        specialArgs = inputs;
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
