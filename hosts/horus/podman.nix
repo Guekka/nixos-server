a881df51bdf90fb739f9a124a87f1c1811d1618f
G'k <39066502+Guekka@users.noreply.github.com>
2023-04-18 09:35:47 +0200

    podman: init

---
+++ hosts/horus/podman.nix
@@ -0,0 +1,13 @@
+{
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.dnsname.enable = true;
+    };
+  };
+}
