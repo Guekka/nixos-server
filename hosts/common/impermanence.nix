08974e35628919ac469410c5049525c7dfad97ca
G'k <39066502+Guekka@users.noreply.github.com>
2023-04-20 13:43:25 +0200

    openssh: init by splitting main configuration

--- hosts/common/impermanence.nix
+++ hosts/common/impermanence.nix
@@ -46,10 +46,6 @@
     ];
     files = [
       "/etc/machine-id"
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
   };
 
