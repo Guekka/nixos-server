6a5ebaea5eadab7ff2db715e8b2f644302505eee
G'k <39066502+Guekka@users.noreply.github.com>
2023-04-20 11:37:34 +0200

    impermanence: init by splitting from main config

--- hosts/horus/default.nix
+++ hosts/horus/default.nix
@@ -2,12 +2,11 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, impermanence, ... }:
+{ config, pkgs, ... }:
 
 {
   imports =
     [
-      impermanence.nixosModule
       ../common
       ./hardware-configuration.nix
       ./nginx.nix
@@ -15,76 +14,6 @@
       ./users.nix
       ./web-server.nix
     ];
-
-  # filesystems
-  fileSystems."/".options = ["compress=zstd" "noatime" ];
-  fileSystems."/home".options = ["compress=zstd" "noatime" ];
-  fileSystems."/nix".options = ["compress=zstd" "noatime" ];
-  fileSystems."/persist".options = ["compress=zstd" "noatime" ];
-  fileSystems."/persist".neededForBoot = true;
-  
-  fileSystems."/var/log".options = ["compress=zstd" "noatime" ];
-  fileSystems."/var/log".neededForBoot = true;
-
-  # reset / at each boot
-  # Note `lib.mkBefore` is used instead of `lib.mkAfter` here.
-  boot.initrd.postDeviceCommands = pkgs.lib.mkBefore ''
-    mkdir -p /mnt
-
-    # We first mount the btrfs root to /mnt
-    # so we can manipulate btrfs subvolumes.
-    mount -o subvol=/ /dev/vda3 /mnt
-
-    # While we're tempted to just delete /root and create
-    # a new snapshot from /root-blank, /root is already
-    # populated at this point with a number of subvolumes,
-    # which makes `btrfs subvolume delete` fail.
-    # So, we remove them first.
-    #
-    # /root contains subvolumes:
-    # - /root/var/lib/portables
-    # - /root/var/lib/machines
-    #
-    # I suspect these are related to systemd-nspawn, but
-    # since I don't use it I'm not 100% sure.
-    # Anyhow, deleting these subvolumes hasn't resulted
-    # in any issues so far, except for fairly
-    # benign-looking errors from systemd-tmpfiles.
-    btrfs subvolume list -o /mnt/root |
-    cut -f9 -d' ' |
-    while read subvolume; do
-      echo "deleting /$subvolume subvolume..."
-      btrfs subvolume delete "/mnt/$subvolume"
-    done &&
-    echo "deleting /root subvolume..." &&
-    btrfs subvolume delete /mnt/root
-
-    echo "restoring blank /root subvolume..."
-    btrfs subvolume snapshot /mnt/root-blank /mnt/root
-
-    # Once we're done rolling back to a blank snapshot,
-    # we can unmount /mnt and continue on the boot process.
-    umount /mnt
-  '';
-
-  # configure impermanence
-  environment.persistence."/persist" = {
-    directories = [
-      "/etc/nixos"
-    ];
-    files = [
-      "/etc/machine-id"
-      "/etc/ssh/ssh_host_ed25519_key"
-      "/etc/ssh/ssh_host_ed25519_key.pub"
-      "/etc/ssh/ssh_host_rsa_key"
-      "/etc/ssh/ssh_host_rsa_key.pub"
-    ];
-  };
-
-  security.sudo.extraConfig = ''
-    # rollback results in sudo lectures after each reboot
-    Defaults lecture = never
-  '';
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
