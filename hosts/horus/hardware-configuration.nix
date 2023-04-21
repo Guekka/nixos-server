fc23cc4679c526ccc98bf01c51ab0d24de33e550
G'k <39066502+Guekka@users.noreply.github.com>
2023-04-21 13:45:50 +0200

    impermanence: make config more generic
    We now assume the disk is labeled with the hostname

--- hosts/horus/hardware-configuration.nix
+++ hosts/horus/hardware-configuration.nix
@@ -11,36 +11,6 @@
 
   boot.initrd.availableKernelModules = ["uhci_hcd" "ehci_pci" "ahci" "sd_mod" "sr_mod"];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/483d59c7-c1f0-4546-8496-9abfed7516f7";
-    fsType = "btrfs";
-    options = ["subvol=root"];
-  };
-
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/483d59c7-c1f0-4546-8496-9abfed7516f7";
-    fsType = "btrfs";
-    options = ["subvol=home"];
-  };
-
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/483d59c7-c1f0-4546-8496-9abfed7516f7";
-    fsType = "btrfs";
-    options = ["subvol=nix"];
-  };
-
-  fileSystems."/persist" = {
-    device = "/dev/disk/by-uuid/483d59c7-c1f0-4546-8496-9abfed7516f7";
-    fsType = "btrfs";
-    options = ["subvol=persist"];
-  };
-
-  fileSystems."/var/log" = {
-    device = "/dev/disk/by-uuid/483d59c7-c1f0-4546-8496-9abfed7516f7";
-    fsType = "btrfs";
-    options = ["subvol=log"];
-  };
-
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/9D4E-2BFF";
     fsType = "vfat";
