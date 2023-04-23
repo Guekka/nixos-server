438f0ed589be2228fc469c107f0c0fa9d47031e9
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-04-23 10:53:02 +0200

    nix: gc, enable nix3

---
+++ hosts/common/global/nix.nix
@@ -0,0 +1,27 @@
+{lib, ...} @ inputs: let
+  flakes = lib.filterAttrs (name: value: value ? outputs) inputs;
+  nixRegistry = lib.mapAttrs (name: v: {flake = v;}) flakes;
+in {
+  nix = {
+    settings = {
+      trusted-users = ["root" "@wheel"];
+      auto-optimise-store = lib.mkDefault true;
+      experimental-features = ["nix-command" "flakes" "repl-flake"];
+      warn-dirty = false;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      # Delete older generations too
+      options = "--delete-older-than 7d";
+    };
+
+    # Add each flake input as a registry
+    # To make nix3 commands consistent with the flake
+    registry = nixRegistry;
+
+    # Add nixpkgs input to NIX_PATH
+    # This lets nix2 commands still use <nixpkgs>
+    nixPath = ["nixpkgs=${inputs.nixpkgs.outPath}"];
+  };
+}
