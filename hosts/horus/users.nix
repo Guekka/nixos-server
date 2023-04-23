a5b91d562034180ac3493241de1a8efdcd82f45e
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-04-23 13:35:33 +0200

    refactor: reduce duplication in users

--- hosts/horus/users.nix
+++ hosts/horus/users.nix
@@ -1,42 +1,15 @@
-{
-  users.mutableUsers = false;
+let
+  mkUser = user: key: {
+    isNormalUser = true;
+    extraGroups = ["wheel"];
+    openssh.authorizedKeys.keys = [key];
+    passwordFile = "/persist/passwords/${user}";
+  };
+in {
   users.users = {
-    edgar = {
-      isNormalUser = true;
-      extraGroups = ["wheel"];
-
-      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFQ4dwdR5kG7RApFSuqiy11IoRG0pECnMLbiLLfttpwJ beelink"];
-
-      # passwordFile needs to be in a volume marked with `neededForBoot = true`
-      passwordFile = "/persist/passwords/edgar";
-    };
-    maxime = {
-      isNormalUser = true;
-      extraGroups = ["wheel"];
-
-      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPspfyaUsaq6jiwQ5mjz4d6Zfd30B7VUOvzqllV0UaFo ozeliurs@gmail.com"];
-      passwordFile = "/persist/passwords/maxime";
-    };
-    tom = {
-      isNormalUser = true;
-      extraGroups = ["wheel"];
-
-      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPoGjjQkOBXDqWS7Sx0cFe+rks6dYkVVt+DgH5Ze0FRj tombe@LAPTOP-O2I1684M"];
-      passwordFile = "/persist/passwords/tom";
-    };
-    raphael = {
-      isNormalUser = true;
-      extraGroups = ["wheel"];
-
-      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICacN1gCNo4bsV9WsZ84ZQQvfs+AfcvYBWzTWhJwqHnJ raphaelanjou@MacBook-Pro-Raphael-Anjou.local"];
-      passwordFile = "/persist/passwords/raphael";
-    };
-    apoorva = {
-      isNormalUser = true;
-      extraGroups = ["wheel"];
-
-      openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPutUy6DkcaM3pP/lcCWdsuNE9czLHJQPF3FrZx/KKUr appad@LAPTOP-V040VGK2"];
-      passwordFile = "/persist/passwords/apoorva";
-    };
+    maxime = mkUser "maxime" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPspfyaUsaq6jiwQ5mjz4d6Zfd30B7VUOvzqllV0UaFo ozeliurs@gmail.com";
+    tom = mkUser "tom" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPoGjjQkOBXDqWS7Sx0cFe+rks6dYkVVt+DgH5Ze0FRj tombe@LAPTOP-O2I1684M";
+    raphael = mkUser "raphael" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICacN1gCNo4bsV9WsZ84ZQQvfs+AfcvYBWzTWhJwqHnJ raphaelanjou@MacBook-Pro-Raphael-Anjou.local";
+    apoorva = mkUser "apoorva" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPutUy6DkcaM3pP/lcCWdsuNE9czLHJQPF3FrZx/KKUr appad@LAPTOP-V040VGK2";
   };
 }
