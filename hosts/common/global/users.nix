95ead6f5b27ce51c68cae7f9517250f18e6e2baf
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-04-26 07:56:30 +0200

    fish: init

--- hosts/common/global/users.nix
+++ hosts/common/global/users.nix
@@ -1,9 +1,10 @@
-{
+{pkgs}: {
   users = {
     mutableUsers = false;
     users = {
       edgar = {
         isNormalUser = true;
+        shell = pkgs.fish;
         extraGroups = ["wheel"];
         openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFQ4dwdR5kG7RApFSuqiy11IoRG0pECnMLbiLLfttpwJ beelink"];
         passwordFile = "/persist/passwords/edgar";
