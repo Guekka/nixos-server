bbface92ddf7035c82739b036d448e1ad768be6b
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-04-26 12:54:46 +0200

    home: initial setup

--- home/edgar/global/default.nix
+++ home/edgar/global/default.nix
@@ -1,7 +1,68 @@
-{config, ...}: {
+{config, pkgs, ...}: {
   home = {
     username = "edgar";
     homeDirectory = "/home/${config.home.username}";
     stateVersion = "22.11";
   };
+
+  # direnv
+  programs.direnv.enable = true;
+  programs.direnv.nix-direnv.enable = true;
+
+  # Add stuff for your user as you see fit:
+  home.packages = with pkgs; [
+    alejandra
+    android-studio
+    anytype
+    bat
+    bottom
+    cmake
+    comma
+    discord
+    duf
+    du-dust
+    difftastic
+    exa
+    fd
+    firefox
+    fzf
+    gcc
+    git
+    gnumake
+    helix
+    hexyl
+    jetbrains.clion
+    jetbrains.idea-ultimate
+    jetbrains.pycharm-professional
+    jetbrains.gateway
+    jq
+    keepassxc
+    kondo
+    libsForQt5.ark
+    maven
+    nix-direnv
+    obsidian
+    python3
+    p7zip
+    qbittorrent
+    qrcp
+    starship
+    tailscale
+    topgrade
+    vlc
+    vscode
+    vim
+    wezterm
+    wireguard-tools
+    wget
+    zoxide
+  ];
+  # Enable home-manager
+  programs.home-manager.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # Configure wezterm
+  programs.wezterm.extraConfig = ''return { color_scheme = "Solarized Light (base16)", }'';
 }
