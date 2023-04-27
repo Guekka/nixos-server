70f22203501b18b2ff19f9da7307cf0a7e6cd21a
G'k <39066502+Guekka@users.noreply.github.com>
2023-04-27 07:53:59 +0200

    gnome: enable extensions, disable default apps

--- hosts/common/optional/gnome.nix
+++ hosts/common/optional/gnome.nix
@@ -11,9 +11,33 @@
     };
   };
 
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      gnome-photos
+      gnome-tour
+    ])
+    ++ (with pkgs.gnome; [
+      cheese # webcam tool
+      gnome-music
+      gedit # text editor
+      epiphany # web browser
+      geary # email reader
+      gnome-characters
+      tali # poker game
+      iagno # go game
+      hitori # sudoku game
+      atomix # puzzle game
+      yelp # Help view
+      gnome-contacts
+      gnome-initial-setup
+    ]);
+
   environment.systemPackages = with pkgs; [
-    unstable.pop-launcher # not yet stabilized
+    gnome.gnome-tweaks
     gnomeExtensions.pop-shell
     gnomeExtensions.pop-launcher-super-key
+    unstable.pop-launcher # not yet stabilized
   ];
+
+  programs.dconf.enable = true;
 }
