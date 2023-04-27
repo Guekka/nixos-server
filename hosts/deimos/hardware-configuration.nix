980eaca4c55737d385173d16b4201497989b20b0
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-04-27 09:39:55 +0200

    deimos: init host (laptop)

---
+++ hosts/deimos/hardware-configuration.nix
@@ -0,0 +1,24 @@
+{
+  hardware,
+  nixpkgs,
+  ...
+}: {
+  imports = [
+    hardware.nixosModules.lenovo-ideapad-15arh05
+    nixpkgs.nixosModules.notDetected
+  ];
+
+  boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci"];
+  boot.kernelModules = ["kvm-amd"];
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/D632-F96F";
+    fsType = "vfat";
+  };
+
+  swapDevices = []; # TODO: add swap
+
+  hardware.cpu.amd.updateMicrocode = true;
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+}
