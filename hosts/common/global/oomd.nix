87428b49424749b076c768ea9ee043e1856dd899
G'k <39066502+Guekka@users.noreply.github.com>
2023-04-28 18:32:41 +0200

    oomd: init

---
+++ hosts/common/global/oomd.nix
@@ -0,0 +1,10 @@
+{
+  systemd.oomd = {
+    enable = true;
+    enableUserServices = true;
+    enableRootSlice = true;
+    extraConfig = {
+      DefaultMemoryPressureDurationSec="10s";
+    };
+  };
+}
