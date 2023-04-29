248772a808888dbd761cdcd22027667d94318a2e
G'k <39066502+Guekka@users.noreply.github.com>
2023-04-29 08:43:02 +0200

    home-manager: downgrade to stable

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
