35ac3f233bffe4a3462a9a9508f7c876833cfc92
G'k <39066502+Guekka@users.noreply.github.com>
2023-05-24 08:53:34 +0200

    qbittorrent: use oci image instead with wireguard
    I wanted to configure wireguard manually, but I do not trust
    myself enough. This image comes with safety rules. Let's hope
    the author did iptables right.

--- hosts/horus/qbittorrent.nix
+++ hosts/horus/qbittorrent.nix
@@ -1,31 +1,59 @@
-{
-  config,
-  outputs,
-  ...
-}: {
-  imports = [outputs.nixosModules.qbittorrent];
-
-  services.qbittorrent = {
-    enable = true;
-    openFirewall = true;
+let
+  uid = 9876;
+  gid = 9877;
+in {
+  virtualisation.oci-containers.containers.qbittorrent = {
+    image = "dyonr/qbittorrentvpn";
+    ports = ["127.0.0.1:8080:8080"];
+    extraOptions = ["--privileged"];
+    volumes = [
+      "/etc/wireguard/:/config/wireguard/"
+      "/var/lib/qbittorrent:/config"
+      "/shared/downloads:/downloads"
+    ];
+    environment = {
+      VPN_ENABLED = "yes";
+      VPN_TYPE = "wireguard";
+      LAN_NETWORK = "192.168.1.0/24";
+      ENABLE_SSL = "no";
+      PUID = toString uid;
+      PGID = toString gid;
+    };
   };
 
   services.nginx.virtualHosts."torrent.bizel.fr" = {
     enableACME = true;
     forceSSL = true;
     locations."^~ /" = {
-      proxyPass = "http://localhost:${toString config.services.qbittorrent.port}";
+      proxyPass = "http://localhost:8080";
     };
   };
 
   environment.persistence."/persist" = {
     directories = [
       {
-        directory = config.services.qbittorrent.dataDir;
-        user = config.services.qbittorrent.user;
-        group = config.services.qbittorrent.group;
-        mode = "0700";
+        directory = "/var/lib/qbittorrent";
+        user = "qbittorrent";
+        group = "qbittorrent";
+        mode = "0750";
+      }
+      {
+        directory = "/etc/wireguard";
+        user = "root";
+        group = "root";
+        mode = "0750";
       }
     ];
+  };
+
+  users = {
+    users.qbittorrent = {
+      inherit uid;
+      group = "qbittorrent";
+      isSystemUser = true;
+    };
+    groups.qbittorrent = {
+      inherit gid;
+    };
   };
 }
