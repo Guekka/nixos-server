ebc62ac08a3d467cf0783f6689a2b3dd88f4d985
G'k <39066502+Guekka@users.noreply.github.com>
2023-05-25 14:21:08 +0200

    *arr: init lidarr, prowlarr, radarr, sonarr
    Too bad these ones use a DB for storing settings

---
+++ hosts/horus/arr/lidarr.nix
@@ -0,0 +1,24 @@
+{
+  services.lidarr = {
+    enable = true;
+  };
+
+  services.nginx.virtualHosts."lidarr.bizel.fr" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."^~ /" = {
+      proxyPass = "http://127.0.0.1:8686";
+    };
+  };
+
+  environment.persistence."/persist" = {
+    directories = [
+      {
+        directory = "/var/lib/lidarr";
+        user = "lidarr";
+        group = "lidarr";
+        mode = "0755";
+      }
+    ];
+  };
+}
