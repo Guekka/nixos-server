83e71073eb927297f0ba37219cb4d99826eaf874
G'k <39066502+Guekka@users.noreply.github.com>
2023-05-27 12:06:27 +0200

    uptime-kuma: init

---
+++ hosts/horus/uptime-kuma.nix
@@ -0,0 +1,33 @@
+{
+  services.uptime-kuma = {
+    enable = true;
+  };
+
+  services.nginx.virtualHosts."status.bizel.fr" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."^~ /" = {
+      proxyPass = "http://127.0.0.1:3001";
+      proxyWebsockets = true;
+    };
+  };
+
+  environment.persistence."/persist" = {
+    directories = [
+      {
+        directory = "/var/lib/private/uptime-kuma";
+        user = "uptime-kuma";
+        group = "uptime-kuma";
+        mode = "0755";
+      }
+    ];
+  };
+
+  users = {
+    users.uptime-kuma = {
+      isSystemUser = true;
+      group = "uptime-kuma";
+    };
+    groups.uptime-kuma = {};
+  };
+}
