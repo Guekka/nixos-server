746b75322161bad20a3922294af1422bc9757050
G'k <39066502+Guekka@users.noreply.github.com>
2023-05-28 08:39:42 +0200

    all: update lock

--- hosts/hestia/amdgpu-fix-sleep.nix
+++ hosts/hestia/amdgpu-fix-sleep.nix
@@ -1,4 +1,4 @@
 # See https://discourse.nixos.org/t/black-screen-when-resuming-from-suspend/10299/2
 {pkgs, ...}: {
-  boot.kernelPackages = pkgs.linuxPackages_6_2;
+  boot.kernelPackages = pkgs.linuxPackages_6_3;
 }
