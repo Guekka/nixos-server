f6353c9ed33373bb13dc5cf7e10a5f44723325e6
G'k <39066502+Guekka@users.noreply.github.com>
2023-05-28 10:10:06 +0200

    keepassxc: force wayland
    This fixes an issue with drag and drop

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,11 +2,15 @@
   # my packages
   additions = final: _prev: import ../pkgs {pkgs = final;};
 
-  modifications = final: _prev: {
+  modifications = final: prev: {
     stable = import inputs.nixpkgs-stable {
       system = final.system;
       config.allowUnfree = true;
     };
+    # fixes an issue with drag and drop
+    keepassxc = prev.keepassxc.overrideAttrs (_old: {
+      postFixup = ''wrapProgram $out/bin/keepassxc --set QT_QPA_PLATFORM wayland'';
+    });
   };
 
   jetbrains = inputs.jetbrains-updater.overlay;
