18763dc9d53d46c5f483712059905bfd50b4da6d
G'k <39066502+Guekka@users.noreply.github.com>
2023-05-31 08:12:26 +0200

    exa: change default settings

--- home/edgar/global/exa.nix
+++ home/edgar/global/exa.nix
@@ -2,5 +2,12 @@
   programs.exa = {
     enable = true;
     enableAliases = true;
+    git = true;
+    extraOptions = [
+      "-lag"
+      "--header"
+      "--icons"
+      "--group-directories-first"
+    ];
   };
 }
