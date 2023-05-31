62255485e117a40b18547d707948b2fe0dde9fef
G'k <39066502+Guekka@users.noreply.github.com>
2023-05-31 08:33:26 +0200

    web-server: add private dir

--- hosts/horus/web-server.nix
+++ hosts/horus/web-server.nix
@@ -1,8 +1,18 @@
-{
+{config, ...}: {
   services.nginx.virtualHosts."e1.oze.li" = {
     addSSL = true;
     enableACME = true;
     root = "/var/www/files";
+
+    locations."/private/" = {
+      basicAuthFile = config.sops.secrets.webServerPrivatePass.path;
+    };
+  };
+
+  sops.secrets.webServerPrivatePass = {
+    sopsFile = ./secrets.yaml;
+    owner = "nginx";
+    group = "nginx";
   };
 
   environment.persistence = {
