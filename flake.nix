51a5bd904cfe34d9ebc36d3023fb0a16320ba3fa
G'k <39066502+Guekka@users.noreply.github.com>
2023-06-02 12:36:06 +0200

    update stable to 23.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     impermanence.url = "github:nix-community/impermanence";
     sops-nix = {
