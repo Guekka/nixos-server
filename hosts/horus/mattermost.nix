646e2d2a1a719603cb497f595f435da1f89f7642
G'k <39066502+Guekka@users.noreply.github.com>
2023-06-28 11:17:25 +0200

    mattermost: init

---
+++ hosts/horus/mattermost.nix
@@ -0,0 +1,33 @@
+{pkgs, ...}: {
+  services.mattermost = {
+    enable = true;
+    siteUrl = "https://mattermost.bizel.fr";
+    preferNixConfig = true;
+    plugins = [
+      (pkgs.fetchurl {
+        url = "https://github.com/mattermost/mattermost-plugin-calls/releases/download/v0.16.1/com.mattermost.calls-0.16.1.tar.gz";
+        hash = "sha256-BCNFmKIxNJnuz0r4SnJn/9apvEjggmTXhJ0EEtFT590=";
+      })
+    ];
+  };
+
+  services.nginx.virtualHosts."mattermost.bizel.fr" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."^~ /" = {
+      proxyPass = "http://127.0.0.1:8065";
+      proxyWebsockets = true;
+    };
+  };
+
+  environment.persistence."/persist" = {
+    directories = [
+      {
+        directory = "/var/lib/mattermost";
+        mode = "0750";
+        user = "mattermost";
+        group = "mattermost";
+      }
+    ];
+  };
+}
