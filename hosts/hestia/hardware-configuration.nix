1a314af98c9cdad2e891c3a62d9fdec54ccc2bf1
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-08-07 14:45:42 +0200

    hestia: move home to root disk, add other drives

--- hosts/hestia/hardware-configuration.nix
+++ hosts/hestia/hardware-configuration.nix
@@ -9,14 +9,21 @@
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
   boot.kernelModules = ["kvm-amd"];
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/02CB-C556";
-    fsType = "vfat";
-  };
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-uuid/02CB-C556";
+      fsType = "vfat";
+    };
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/acbf6beb-7d17-407d-8ef0-3202477912fc";
-    fsType = "ext4";
+    "/sandisk" = {
+      device = "/dev/disk/by-uuid/bcfed9b2-8c80-4cf4-80b4-a6c1c221638d";
+      fsType = "btrfs";
+    };
+
+    "/data" = {
+      device = "/dev/disk/by-uuid/acbf6beb-7d17-407d-8ef0-3202477912fc";
+      fsType = "ext4";
+    };
   };
 
   swapDevices = [
