4e2f86a10e0d3a5630706358eaeabd04c21c2e47
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-08-27 22:52:39 +0200

    monitors: init hm module

--- hosts/common/global/home-manager.nix
+++ hosts/common/global/home-manager.nix
@@ -1,7 +1,14 @@
-{inputs, ...}: {
+{
+  inputs,
+  outputs,
+  ...
+}: {
   imports = [inputs.home-manager.nixosModules.home-manager];
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
+    extraSpecialArgs = {
+      inherit inputs outputs;
+    };
   };
 }
