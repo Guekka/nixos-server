f86f93d80f7119e5bbc3b6b4ea7386abf88fbb58
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-08-28 12:52:27 +0200

    monitors: configure hestia

--- home/edgar/hestia.nix
+++ home/edgar/hestia.nix
@@ -4,4 +4,24 @@
     ./global
     ./gui
   ];
+
+  monitors = [
+    {
+      name = "HDMI-A-1";
+      width = 2560;
+      height = 1440;
+      noBar = true;
+      x = 0;
+      workspace = "2";
+    }
+    {
+      name = "DP-2";
+      width = 2560;
+      height = 1440;
+      refreshRate = 144;
+      x = 2560;
+      workspace = "1";
+      primary = true;
+    }
+  ];
 }
