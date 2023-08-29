6e37cd35cb11cdd3dd2f3925bf1576179c78d109
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-08-29 09:13:48 +0200

    hyprland: fix some xdg issues

--- hosts/common/optional/hyprland.nix
+++ hosts/common/optional/hyprland.nix
@@ -1,3 +1,27 @@
-{
+{pkgs, ...}: {
   programs.hyprland.enable = true;
+
+  # XDG Portals
+  xdg = {
+    portal = {
+      enable = true;
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+      ];
+    };
+  };
+
+  environment.systemPackages = [
+    pkgs.xdg-utils # xdg-open
+  ];
+
+  # Mostly from <https://www.reddit.com/r/NixOS/comments/137j18j/comment/ju6h25k/>
+  environment.sessionVariables = {
+    # NIXOS_OZONE_WL = "1"; disable for now since vs code is broken
+    SDL_VIDEODRIVER = "wayland";
+    _JAVA_AWT_WM_NONREPARENTING = "1";
+    CLUTTER_BACKEND = "wayland";
+    WLR_RENDERER = "vulkan";
+    QT_QPA_PLATFORM = "wayland";
+  };
 }
