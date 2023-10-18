f61dd28b4d91cee11f8c6df07277d9dd10cbcd78
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-10-18 18:37:10 +0200

    git: update config

--- home/edgar/global/git.nix
+++ home/edgar/global/git.nix
@@ -7,7 +7,34 @@
 
     extraConfig = {
       feature.manyFiles = true;
+      # see https://github.com/tummychow/git-absorb/issues/81
+      index.skipHash = false;
+
       init.defaultBranch = "main";
+
+      # preserve original committer when rebasing. See <https://stackoverflow.com/a/76325489/10796945>
+      fetch = {
+        prune = true;
+      };
+
+      help = {
+        autocorrect = 3; # tenths of second before running
+      };
+
+      pull = {
+        rebase = true;
+      };
+
+      push = {
+        autoSetupRemote = true;
+        followTags = true;
+      };
+
+      rebase = {
+        instructionFormat = "%s%nexec GIT_COMMITTER_DATE=\"%cI\" GIT_COMMITTER_NAME=\"%cN\" GIT_COMMITTER_EMAIL=\"%cE\" git commit --amend --no-edit --allow-empty --allow-empty-message%n";
+      };
+
+      push.autoSetupRemote = true;
     };
     ignores = [".direnv" "result"];
 
