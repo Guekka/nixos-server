788ef7090ee68b0d0b7f3d77d49602d0c4bbdcb3
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-10-28 12:29:39 +0200

    users: fallback if passwordFile does not exist

--- hosts/common/global/users.nix
+++ hosts/common/global/users.nix
@@ -2,7 +2,9 @@
   pkgs,
   config,
   ...
-}: {
+}: let
+  passwordFile = "/persist/passwords/edgar";
+in {
   programs.fish.enable = true;
   users = {
     mutableUsers = false;
@@ -12,7 +14,7 @@
         shell = pkgs.fish;
         extraGroups = ["wheel"];
         openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFQ4dwdR5kG7RApFSuqiy11IoRG0pECnMLbiLLfttpwJ beelink"];
-        hashedPasswordFile = "/persist/passwords/edgar";
+        hashedPasswordFile = passwordFile;
       };
     };
   };
@@ -20,6 +22,19 @@
   home-manager.users.edgar = import home/${config.networking.hostName}.nix;
 
   security.pam.services.swaylock = {};
+
+  # I've locked myself out of my computer too many times when the password file doesn't exist
+  # So create a default one
+  system.activationScripts.createDefaultPasswordFile = ''
+    mkdir -p /persist/passwords
+    # only create the file if it doesn't exist. Password is "hunter2"
+    if [ ! -f ${passwordFile} ]; then
+      echo "${passwordFile} doesn't exist, creating it. Password is \"hunter2\""
+      echo '$6$bH8SwzNFISaaeQ6g$anQ2PpZy1l8Mkr/TeDgLnOqchTnR9yHA1Pfh0o1HP8KFk4B4Ei6mhGRmyV/0fa.k9jkBJf2sz97OUOhNLdMPn/' > ${passwordFile}
+      # what if the update users script runs before this one? Let's change the password in case
+      yes hunter2 | passwd edgar
+    fi
+  '';
 
   # Home secrets. I guess the proper way would be to use the home manager module
   # But it seems too tedious
