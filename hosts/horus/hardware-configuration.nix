d86894e53f8ed0d6f91f01ddaeb96d7f76b8c1ab
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-11-01 22:16:52 +0100

    horus: fix swapfile location

--- hosts/horus/hardware-configuration.nix
+++ hosts/horus/hardware-configuration.nix
@@ -5,11 +5,13 @@
 
   boot.initrd.availableKernelModules = ["uhci_hcd" "ehci_pci" "ahci" "sd_mod" "sr_mod"];
 
-  swapDevices = {
-    "/swapfile" = {
+  swapDevices = [
+    {
+      # on /persist since / is tmpfs. It woudln't make sense to have a swapfile in tmpfs
+      device = "/persist/swapfile";
       size = 8192;
-    };
-  };
+    }
+  ];
 
   nixpkgs.hostPlatform = "aarch64-linux";
   hardware.cpu.amd.updateMicrocode = true;
