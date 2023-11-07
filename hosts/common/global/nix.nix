5a4794b81a8526923a3d81130799e79e33216d01
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-11-07 08:46:36 +0100

    nix: add more caches

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -12,6 +12,15 @@ in {
       auto-optimise-store = lib.mkDefault true;
       experimental-features = ["nix-command" "flakes" "repl-flake"];
       warn-dirty = false;
+
+      substituters = [
+        "https://nix-community.cachix.org"
+        "https://cuda-maintainers.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      ];
     };
     gc = {
       automatic = true;
