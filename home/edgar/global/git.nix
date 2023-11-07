486d8e6fcb6c8712a44aaea7a7e92c532008f172
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-11-07 13:40:53 +0100

    git: update config

--- home/edgar/global/git.nix
+++ home/edgar/global/git.nix
@@ -5,6 +5,27 @@
     userName = "Edgar B";
     userEmail = "39066502+Guekka@users.noreply.github.com";
 
+    aliases = {
+      a = "add";
+      br = "branch";
+
+      # Show the current branch name (useful for shell prompts)
+      brname = "!git branch | grep '^*' | awk '{ print $2 }'";
+
+      cm = "commit -m";
+      cma = "commit --amend --no-edit";
+
+      dfs = "diff --staged";
+
+      # more compact log
+      l = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'";
+
+      pf = "push  --force-with-lease";
+      s = "status";
+      st = "stash --include-untracked";
+      sw = "switch";
+    };
+
     extraConfig = {
       feature.manyFiles = true;
       # see https://github.com/tummychow/git-absorb/issues/81
@@ -34,9 +55,37 @@
         instructionFormat = "%s%nexec GIT_COMMITTER_DATE=\"%cI\" GIT_COMMITTER_NAME=\"%cN\" GIT_COMMITTER_EMAIL=\"%cE\" git commit --amend --no-edit --allow-empty --allow-empty-message%n";
       };
 
-      push.autoSetupRemote = true;
+      help = {
+        autocorrect = true;
+      };
+
+      pull = {
+        rebase = true;
+      };
+
+      push = {
+        autoSetupRemote = true;
+        followTags = true;
+      };
+
+      rebase = {
+        instructionFormat = "%s%nexec GIT_COMMITTER_DATE=\"%cI\" GIT_COMMITTER_NAME=\"%cN\" GIT_COMMITTER_EMAIL=\"%cE\" git commit --amend --no-edit --allow-empty --allow-empty-message%n";
+
+        autoStash = true;
+        autoSquash = true;
+      };
+
+      rerere = {
+        enabled = true;
+        autoUpdate = true;
+      };
+
+      status = {
+        showUntrackedFiles = "all";
+      };
     };
-    ignores = [".direnv" "result"];
+
+    ignores = [".devenv" ".direnv" "result"];
 
     difftastic = {
       enable = true;
