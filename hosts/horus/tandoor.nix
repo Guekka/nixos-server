272ea7d0efc2ec3342379849f8d7164d053e9c39
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-11-11 20:09:17 +0100

    horus: remove some users
    Signed-off-by: Edgar B <39066502+Guekka@users.noreply.github.com>

---
+++ hosts/horus/tandoor.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  services.tandoor-recipes = {
+    enable = true;
+    package = pkgs.stable.tandoor-recipes; # unstable doesn't build
+    port = 7453;
+    #address = "tandoor.bizel.fr";
+  };
+
+  services.nginx.virtualHosts."tandoor.bizel.fr" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."^~ /" = {
+      proxyPass = "http://localhost:${toString config.services.tandoor-recipes.port}";
+    };
+  };
+
+  environment.persistence."/persist".directories = [
+    {
+      directory = "/var/lib/private/tandoor-recipes";
+      user = "tandoor_recipes";
+      group = "tandoor_recipes";
+    }
+  ];
+}
