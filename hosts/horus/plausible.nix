1a163de50a51f61601af22671ad2a94a711450d6
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-11-22 08:51:16 +0100

    clickhouse: apply plausible recommended config
    Signed-off-by: Edgar B <39066502+Guekka@users.noreply.github.com>

--- hosts/horus/plausible.nix
+++ hosts/horus/plausible.nix
@@ -23,6 +23,37 @@
     };
   };
 
+  environment.etc = {
+    # From https://github.com/plausible/hosting/tree/master/clickhouse
+    "clickhouse-server/config.d/nologs.xml".text = ''
+      <clickhouse>
+          <logger>
+              <level>warning</level>
+              <console>true</console>
+          </logger>
+          <query_thread_log remove="remove"/>
+          <query_log remove="remove"/>
+          <text_log remove="remove"/>
+          <trace_log remove="remove"/>
+          <metric_log remove="remove"/>
+          <asynchronous_metric_log remove="remove"/>
+          <session_log remove="remove"/>
+          <part_log remove="remove"/>
+      </clickhouse>
+    '';
+
+    "clickhouse-server/users.d/nologs.xml".text = ''
+      <clickhouse>
+          <profiles>
+              <default>
+                  <log_queries>0</log_queries>
+                  <log_query_threads>0</log_query_threads>
+              </default>
+          </profiles>
+      </clickhouse>
+    '';
+  };
+
   environment.persistence."/persist" = {
     directories = [
       {
