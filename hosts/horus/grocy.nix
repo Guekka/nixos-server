8f1426de052b034e0e56b72b2fc3df2b44b95fdc
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-11-25 09:48:01 +0100

    grocy: add persistence
    Signed-off-by: Edgar B <39066502+Guekka@users.noreply.github.com>

--- hosts/horus/grocy.nix
+++ hosts/horus/grocy.nix
@@ -1,10 +1,15 @@
-{
+{config, ...}: {
   services.grocy = {
     enable = true;
     hostName = "grocy.bizel.fr";
+
     settings = {
       currency = "EUR";
       culture = "fr";
     };
+  };
+
+  environment.persistence."/persist" = {
+    directories = [config.services.grocy.dataDir];
   };
 }
