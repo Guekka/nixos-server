4e5d9f5a7882a629446a30d518ff879587457de3
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-11-30 23:03:22 +0100

    bottom: customize

--- home/edgar/global/bottom.nix
+++ home/edgar/global/bottom.nix
@@ -1,5 +1,13 @@
 {
   programs.bottom = {
     enable = true;
+    settings = {
+      flags = {
+        battery = true;
+        color = "default-light";
+        tree = true;
+        enable_cache_memory = true;
+      };
+    };
   };
 }
