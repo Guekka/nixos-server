9178d3109d6c05f37b96ee16d3702efcf2d3a087
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-12-01 11:08:42 +0100

    hyprland: default rule for unknown monitors

--- home/edgar/gui/hyprland/default.nix
+++ home/edgar/gui/hyprland/default.nix
@@ -212,16 +212,18 @@
             "SUPER,semicolon,exec,pass-wofi"
           ]));
 
-      monitor = map (
-        m: let
-          resolution = "${toString m.width}x${toString m.height}@${toString m.refreshRate}";
-          position = "${toString m.x}x${toString m.y}";
-        in "${m.name},${
-          if m.enabled
-          then "${resolution},${position},1"
-          else "disable"
-        }"
-      ) (config.monitors);
+      monitor =
+        map (
+          m: let
+            resolution = "${toString m.width}x${toString m.height}@${toString m.refreshRate}";
+            position = "${toString m.x}x${toString m.y}";
+          in "${m.name},${
+            if m.enabled
+            then "${resolution},${position},1"
+            else "disable"
+          }"
+        ) (config.monitors)
+        ++ [",preferred,auto,1"];
 
       workspace = map (
         m: "${m.name},${m.workspace}"
