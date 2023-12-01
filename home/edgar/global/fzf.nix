1d10f735bc480573d229e4038d1b7545aff49500
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-12-01 14:53:50 +0100

    fzf: follow nix-colors theme

--- home/edgar/global/fzf.nix
+++ home/edgar/global/fzf.nix
@@ -1,10 +1,14 @@
-{
+{config, ...}: {
   programs.fzf = {
     enable = true;
     enableBashIntegration = true;
     enableFishIntegration = true;
-    defaultOptions = [
-      "--color=light"
+
+    defaultOptions = with config.colorscheme.colors; [
+      # https://github.com/sagikazarmark/nix-config/blob/f1380179eae6082e6383fe071982c458041cda85/modules/home-manager/modules/nix-colors/programs/fzf.nix
+      "--color=bg+:#${base01},bg:#${base00},spinner:#${base0C},hl:#${base0D}"
+      "--color=fg:#${base04},header:#${base0D},info:#${base0A},pointer:#${base0C}"
+      "--color=marker:#${base0C},fg+:#${base06},prompt:#${base0A},hl+:#${base0D}"
     ];
   };
 }
