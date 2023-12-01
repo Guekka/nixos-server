a2abbfb91575745bb5a61610f292ad7be172ca0d
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-12-01 14:37:11 +0100

    bottom: follow nix-colors theme

--- home/edgar/global/bottom.nix
+++ home/edgar/global/bottom.nix
@@ -1,7 +1,32 @@
-{
+{config, ...}: {
   programs.bottom = {
     enable = true;
     settings = {
+      # from <https://github.com/guifuentes8/nix-config/blob/main/home/global/interfaces/WM/shared/pkgs/bottom/default.nix>
+      colors = with config.colorScheme; {
+        table_header_color = "#${colors.base06}";
+        all_cpu_color = "#${colors.base06}";
+        avg_cpu_color = "#${colors.base0F}";
+        cpu_core_colors = ["#${colors.base08}" "#${colors.base09}" "#${colors.base0A}" "#${colors.base0B}" "#${colors.base0D}" "#${colors.base0E}"];
+        ram_color = "#${colors.base0B}";
+        swap_color = "#${colors.base09}";
+        rx_color = "#${colors.base0B}";
+        tx_color = "#${colors.base08}";
+        widget_title_color = "#${colors.base06}";
+        border_color = "#${colors.base01}";
+        highlighted_border_color = "#${colors.base0F}";
+        text_color = "#${colors.base05}";
+        graph_color = "#${colors.base04}";
+        cursor_color = "#${colors.base0F}";
+        selected_text_color = "#${colors.base02}";
+        selected_bg_color = "#${colors.base0E}";
+        high_battery_color = "#${colors.base0B}";
+        medium_battery_color = "#${colors.base0A}";
+        low_battery_color = "#${colors.base08}";
+        gpu_core_colors = ["#${colors.base0D}" "#${colors.base0E}" "#${colors.base08}" "#${colors.base09}" "#${colors.base0A}" "#${colors.base0B}"];
+        arc_color = "#${colors.base0C}";
+      };
+
       flags = {
         battery = true;
         color = "default-light";
