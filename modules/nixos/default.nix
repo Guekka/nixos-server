c5babf56fd26a4c817d2c38ea5e059c9b4c4e42c
Edgar B <39066502+Guekka@users.noreply.github.com>
2023-12-03 10:34:44 +0100

    nixos(cozy-cloud): init module

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,5 +1,6 @@
 {
   couchdb-extended = import ./couchdb-extended.nix;
+  cozy-cloud = import ./cozy-cloud.nix;
   qbittorrent = import ./qbittorrent.nix;
   tailscale-autoconnect = import ./tailscale-autoconnect.nix;
 }
