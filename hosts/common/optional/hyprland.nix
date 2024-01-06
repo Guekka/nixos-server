902199aeb9f35d0e50b9f8436cf4d2d5230fdf11
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-01-06 13:24:39 +0100

    all: update (and move to hyprland flake)

--- hosts/common/optional/hyprland.nix
+++ hosts/common/optional/hyprland.nix
@@ -1,5 +1,13 @@
-{pkgs, ...}: {
-  programs.hyprland.enable = true;
+{
+  inputs,
+  pkgs,
+  ...
+}: {
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
+  };
 
   # XDG Portals
   xdg = {
