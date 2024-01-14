36f0e7ae8f93a9c54e07ff862820067e15b2e766
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-01-14 16:36:51 +0100

    nginx: add catch-all

--- hosts/horus/nginx.nix
+++ hosts/horus/nginx.nix
@@ -7,5 +7,14 @@
     recommendedOptimisation = true;
     clientMaxBodySize = "300m";
   };
+
   networking.firewall.allowedTCPPorts = [80 443];
+
+  # catch-all
+  services.nginx.virtualHosts."_" = {
+    useACMEHost = "bizel.fr";
+    forceSSL = true;
+    default = true;
+    locations."~ .*".return = "403";
+  };
 }
