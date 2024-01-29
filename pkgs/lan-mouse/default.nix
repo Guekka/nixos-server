fbfc0486f9e0acff820fac6282fe2c05b614f6b9
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-01-29 19:03:32 +0100

    lan-mouse: upgrade

--- pkgs/lan-mouse/default.nix
+++ pkgs/lan-mouse/default.nix
@@ -11,21 +11,27 @@
   libadwaita,
   pango,
   stdenv,
+  darwin,
   wayland,
   xorg,
 }:
 rustPlatform.buildRustPackage rec {
   pname = "lan-mouse";
-  version = "0.3.3";
+  version = "0.6.0";
 
   src = fetchFromGitHub {
     owner = "ferdinandschober";
     repo = "lan-mouse";
     rev = "v${version}";
-    hash = "sha256-prQCzj3/eX0409eEOmM6h4ZbcL/yBrs404EhGMKeXhs=";
+    hash = "sha256-98n0Y9oL/ll90NKHJC/25wkav9K+eVqrO7PlrJMoGmY=";
   };
 
-  cargoHash = "sha256-NqL2MGaxEekEWCtxg0oYxB9FJIBlEX74pErbLfngqDk=";
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "reis-0.1.0" = "sha256-sRZqm6QdmgqfkTjEENV8erQd+0RL5z1+qjdmY18W3bA=";
+    };
+  };
 
   nativeBuildInputs = [
     pkg-config
@@ -41,6 +47,9 @@ rustPlatform.buildRustPackage rec {
       libadwaita
       pango
     ]
+    ++ lib.optionals stdenv.isDarwin [
+      darwin.apple_sdk.frameworks.CoreGraphics
+    ]
     ++ lib.optionals stdenv.isLinux [
       wayland
       xorg.libX11
@@ -48,7 +57,7 @@ rustPlatform.buildRustPackage rec {
     ];
 
   meta = with lib; {
-    description = "Software KVM Switch / mouse & keyboard sharing software for Local Area Networks";
+    description = "Mouse & keyboard sharing via LAN";
     homepage = "https://github.com/ferdinandschober/lan-mouse";
     license = licenses.gpl3Only;
     maintainers = with maintainers; [];
