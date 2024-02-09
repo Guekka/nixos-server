f53263daac19b47d51ba1b7a612330d8334f7868
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-02-09 13:10:42 +0100

    home/anyrun: init and replace wofi

--- flake.nix
+++ flake.nix
@@ -5,6 +5,11 @@
 
     nixpkgs-ida.url = "github:lourkeur/nixpkgs/idafree";
 
+    anyrun = {
+      url = "github:Kirottu/anyrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     impermanence.url = "github:nix-community/impermanence";
     sops-nix = {
       url = "github:mic92/sops-nix";
