b058ebd4e569d61caea9c09b014480e9df18a4de
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-02-11 18:55:59 +0100

    fileshare: add samba

--- hosts/common/optional/fileshare-client.nix
+++ hosts/common/optional/fileshare-client.nix
@@ -1,17 +1,30 @@
-{
-  systemd = {
-    mounts = [
-      {
-        type = "nfs4";
-        what = "horus:/shared";
-        where = "/shared";
-      }
+{config, ...}: {
+  fileSystems = let
+    systemdMountOptions = [
+      "x-systemd.automount"
+      "x-systemd.mount-timeout=3"
     ];
-    automounts = [
-      {
-        where = "/shared";
-        wantedBy = ["default.target"];
-      }
-    ];
+  in {
+    "/shared" = {
+      device = "horus:/shared";
+      fsType = "nfs4";
+      options = systemdMountOptions;
+    };
+
+    "/samba" = {
+      device = "//192.168.1.254/Disque dur";
+      fsType = "cifs";
+      options =
+        [
+          "vers=2.1"
+          "iocharset=utf8"
+          "user=samba_user"
+          "credentials=${config.sops.secrets.samba_credentials.path}"
+          "rw"
+        ]
+        ++ systemdMountOptions;
+    };
   };
+
+  sops.secrets.samba_credentials.sopsFile = ../secrets.yaml;
 }
