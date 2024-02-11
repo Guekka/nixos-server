643c89bbd60246e05778ffa70733c6b5a9036c78
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-02-11 18:14:54 +0100

    home/activitywatch: refactor and awatcher

--- home/edgar/optional/activitywatch.nix
+++ home/edgar/optional/activitywatch.nix
@@ -1,9 +1,54 @@
 {pkgs, ...}: {
-  services.activitywatch = {
-    enable = true;
-    watchers = {
-      aw-watcher-afk.package = pkgs.activitywatch;
-      aw-watcher-window-wayland.package = pkgs.aw-watcher-window-wayland;
+  home.packages = [pkgs.awatcher pkgs.activitywatch];
+  systemd.user.targets.activitywatch = {
+    Unit = {
+      Description = "ActivityWatch server";
+      Requires = ["default.target"];
+      After = ["default.target"];
+    };
+
+    Install.WantedBy = ["default.target"];
+  };
+  systemd.user.services = let
+    baseSettings = {
+      Restart = "on-failure";
+
+      # Some sandboxing.
+      LockPersonality = true;
+      NoNewPrivileges = true;
+      RestrictNamespaces = true;
+    };
+  in {
+    activitywatch = {
+      Unit = {
+        Description = "ActivityWatch time tracker server";
+        Documentation = ["https://docs.activitywatch.net"];
+        BindsTo = ["activitywatch.target"];
+      };
+
+      Service =
+        {
+          ExecStart = "${pkgs.aw-server-rust}/bin/aw-server";
+        }
+        // baseSettings;
+
+      Install.WantedBy = ["activitywatch.target"];
+    };
+
+    awatcher = {
+      Unit = {
+        Description = "awatcher";
+        After = ["activitywatch.service"];
+        BindsTo = ["activitywatch.target"];
+      };
+
+      Service =
+        {
+          ExecStart = "${pkgs.awatcher}/bin/awatcher";
+        }
+        // baseSettings;
+
+      Install.WantedBy = ["activitywatch.target"];
     };
   };
 }
