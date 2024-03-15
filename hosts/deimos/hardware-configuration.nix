50829d6618aaf22a385eca2a7c042b671c626141
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-03-15 15:06:54 +0100

    deimos: add swap

--- hosts/deimos/hardware-configuration.nix
+++ hosts/deimos/hardware-configuration.nix
@@ -5,14 +5,20 @@
   ];
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "ahci"];
-  boot.kernelModules = ["kvm-amd"];
+  boot.kernelModules = ["kvm-amd" "nvidia" "nvidia-uvm"];
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/D632-F96F";
     fsType = "vfat";
   };
 
-  swapDevices = []; # TODO: add swap
+  swapDevices = [
+    {
+      # on /persist since / is tmpfs. It woudln't make sense to have a swapfile in tmpfs
+      device = "/persist/swapfile";
+      size = 8192;
+    }
+  ];
 
   hardware.cpu.amd.updateMicrocode = true;
 
