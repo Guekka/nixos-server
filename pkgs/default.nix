095749f240a99c22d63f80077854eae23976daa8
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-03-22 16:09:23 +0100

    lan-mouse: switch to nixpkgs pkg

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,5 @@
   clight-gui = pkgs.callPackage ./clight-gui {};
   cozy-stack = pkgs.callPackage ./cozy-stack {};
   image-sieve = pkgs.callPackage ./image-sieve {};
-  lan-mouse = pkgs.callPackage ./lan-mouse {};
   tcount = pkgs.callPackage ./tcount {};
 }
