3639206919c7f7ab091dfe184249d4d427864f8d
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-05-24 17:54:25 +0100

    all: upgrade nixpkgs-stable to 24.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     impermanence.url = "github:nix-community/impermanence";
     sops-nix = {
