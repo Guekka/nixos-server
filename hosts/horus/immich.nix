e8a89b6b67c2e952649671bef4c7c5fdaa1431c9
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-06-09 10:51:41 +0100

    immich: add db dumps

--- hosts/horus/immich.nix
+++ hosts/horus/immich.nix
@@ -11,6 +11,8 @@
   postgresRoot = "${immichAppdataRoot}/pgsql";
   postgresUser = "immich";
   postgresDb = "immich";
+
+  immichDatabaseDumps = "/var/backup/immich/db_dumps";
 in {
   services.nginx.virtualHosts."${immichHost}" = {
     extraConfig = ''
@@ -38,93 +40,121 @@ in {
   # - login with the admin user
   # - set the "Machine Learning Settings" > "URL" to http://immich_machine_learning:3003
 
-  virtualisation.oci-containers.containers.immich_server = {
-    image = "ghcr.io/immich-app/immich-server:${immichVersion}";
-    ports = ["127.0.0.1:2283:3001"];
-    extraOptions = [
-      "--pull=newer"
-      # Force DNS resolution to only be the podman dnsname name server; by default podman provides a resolv.conf
-      # that includes both this server and the upstream system server, causing resolutions of other pod names
-      # to be inconsistent.
-      "--dns=10.88.0.1"
-    ];
-    cmd = ["start.sh" "immich"];
-    environment = {
-      IMMICH_VERSION = immichVersion;
-      DB_HOSTNAME = "immich_postgres";
-      DB_USERNAME = postgresUser;
-      DB_DATABASE_NAME = postgresDb;
-      REDIS_HOSTNAME = "immich_redis";
+  virtualisation.oci-containers.containers = {
+    immich_server = {
+      image = "ghcr.io/immich-app/immich-server:${immichVersion}";
+      ports = ["127.0.0.1:2283:3001"];
+      extraOptions = [
+        "--pull=newer"
+        # Force DNS resolution to only be the podman dnsname name server; by default podman provides a resolv.conf
+        # that includes both this server and the upstream system server, causing resolutions of other pod names
+        # to be inconsistent.
+        "--dns=10.88.0.1"
+      ];
+      cmd = ["start.sh" "immich"];
+      environment = {
+        IMMICH_VERSION = immichVersion;
+        DB_HOSTNAME = "immich_postgres";
+        DB_USERNAME = postgresUser;
+        DB_DATABASE_NAME = postgresDb;
+        REDIS_HOSTNAME = "immich_redis";
+      };
+      environmentFiles = [
+        config.sops.secrets.immich_postgres.path
+      ];
+      volumes = [
+        "${immichPhotos}:/usr/src/app/upload"
+        "${immichLibrary}:/usr/src/app/upload/library"
+        "/etc/localtime:/etc/localtime:ro"
+      ];
     };
-    environmentFiles = [
-      config.sops.secrets.immich_postgres.path
-    ];
-    volumes = [
-      "${immichPhotos}:/usr/src/app/upload"
-      "${immichLibrary}:/usr/src/app/upload/library"
-      "/etc/localtime:/etc/localtime:ro"
-    ];
-  };
 
-  virtualisation.oci-containers.containers.immich_microservices = {
-    image = "ghcr.io/immich-app/immich-server:${immichVersion}";
-    extraOptions = [
-      "--pull=newer"
-      # Force DNS resolution to only be the podman dnsname name server; by default podman provides a resolv.conf
-      # that includes both this server and the upstream system server, causing resolutions of other pod names
-      # to be inconsistent.
-      "--dns=10.88.0.1"
-    ];
-    cmd = ["start.sh" "microservices"];
-    environment = {
-      IMMICH_VERSION = immichVersion;
-      DB_HOSTNAME = "immich_postgres";
-      DB_USERNAME = postgresUser;
-      DB_DATABASE_NAME = postgresDb;
-      REDIS_HOSTNAME = "immich_redis";
+    immich_microservices = {
+      image = "ghcr.io/immich-app/immich-server:${immichVersion}";
+      extraOptions = [
+        "--pull=newer"
+        # Force DNS resolution to only be the podman dnsname name server; by default podman provides a resolv.conf
+        # that includes both this server and the upstream system server, causing resolutions of other pod names
+        # to be inconsistent.
+        "--dns=10.88.0.1"
+      ];
+      cmd = ["start.sh" "microservices"];
+      environment = {
+        IMMICH_VERSION = immichVersion;
+        DB_HOSTNAME = "immich_postgres";
+        DB_USERNAME = postgresUser;
+        DB_DATABASE_NAME = postgresDb;
+        REDIS_HOSTNAME = "immich_redis";
+      };
+      environmentFiles = [
+        config.sops.secrets.immich_postgres.path
+      ];
+      volumes = [
+        "${immichPhotos}:/usr/src/app/upload"
+        "${immichLibrary}:/usr/src/app/upload/library"
+        "/etc/localtime:/etc/localtime:ro"
+      ];
     };
-    environmentFiles = [
-      config.sops.secrets.immich_postgres.path
-    ];
-    volumes = [
-      "${immichPhotos}:/usr/src/app/upload"
-      "${immichLibrary}:/usr/src/app/upload/library"
-      "/etc/localtime:/etc/localtime:ro"
-    ];
-  };
 
-  virtualisation.oci-containers.containers.immich_machine_learning = {
-    image = "ghcr.io/immich-app/immich-machine-learning:${immichVersion}";
-    extraOptions = ["--pull=newer"];
-    environment = {
-      IMMICH_VERSION = immichVersion;
+    immich_machine_learning = {
+      image = "ghcr.io/immich-app/immich-machine-learning:${immichVersion}";
+      extraOptions = ["--pull=newer"];
+      environment = {
+        IMMICH_VERSION = immichVersion;
+      };
+      volumes = [
+        "${immichAppdataRoot}/model-cache:/cache"
+      ];
     };
-    volumes = [
-      "${immichAppdataRoot}/model-cache:/cache"
-    ];
-  };
 
-  virtualisation.oci-containers.containers.immich_redis = {
-    image = "registry.hub.docker.com/library/redis:6.2-alpine@sha256:84882e87b54734154586e5f8abd4dce69fe7311315e2fc6d67c29614c8de2672";
-  };
-
-  virtualisation.oci-containers.containers.immich_postgres = {
-    image = "registry.hub.docker.com/tensorchord/pgvecto-rs:pg14-v0.2.0@sha256:90724186f0a3517cf6914295b5ab410db9ce23190a2d9d0b9dd6463e3fa298f0";
-    environment = {
-      POSTGRES_USER = postgresUser;
-      POSTGRES_DB = postgresDb;
+    immich_redis = {
+      image = "registry.hub.docker.com/library/redis:6.2-alpine@sha256:84882e87b54734154586e5f8abd4dce69fe7311315e2fc6d67c29614c8de2672";
     };
-    environmentFiles = [
-      config.sops.secrets.immich_postgres.path
-    ];
-    volumes = [
-      "${postgresRoot}:/var/lib/postgresql/data"
-    ];
+
+    immich_postgres = {
+      image = "registry.hub.docker.com/tensorchord/pgvecto-rs:pg14-v0.2.0@sha256:90724186f0a3517cf6914295b5ab410db9ce23190a2d9d0b9dd6463e3fa298f0";
+      environment = {
+        POSTGRES_USER = postgresUser;
+        POSTGRES_DB = postgresDb;
+      };
+      environmentFiles = [
+        config.sops.secrets.immich_postgres.path
+      ];
+      volumes = [
+        "${postgresRoot}:/var/lib/postgresql/data"
+      ];
+    };
+
+    immich_db_dumper = {
+      image = "prodrigestivill/postgres-backup-local:14";
+      environmentFiles = [
+        config.sops.secrets.immich_postgres.path
+      ];
+      environment = {
+        POSTGRES_HOST = "immich_postgres";
+        POSTGRES_CLUSTER = "TRUE";
+        POSTGRES_USER = postgresUser;
+        POSTGRES_DB = postgresDb;
+        SCHEDULE = "@hourly";
+        POSTGRES_EXTRA_OPTS = "--clean --if-exists";
+        BACKUP_DIR = "/db_dumps";
+      };
+      volumes = [
+        "${immichDatabaseDumps}:/db_dumps"
+      ];
+      dependsOn = ["immich_postgres"];
+    };
   };
 
   environment.persistence."/persist".directories = [
     {
       directory = immichRoot;
+      user = "immich";
+      group = "immich";
+      mode = "0750";
+    }
+    {
+      directory = immichDatabaseDumps;
       user = "immich";
       group = "immich";
       mode = "0750";
