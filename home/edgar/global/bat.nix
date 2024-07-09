7abdb788321cbb370ddee11ec82c02d6fcc54c23
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-07-09 21:04:42 +0100

    bat: enable extras

--- home/edgar/global/bat.nix
+++ home/edgar/global/bat.nix
@@ -1,6 +1,7 @@
-{
+{pkgs, ...}: {
   programs.bat = {
     enable = true;
     config.theme = "base16";
+    extraPackages = with pkgs.bat-extras; [batman batgrep batwatch batpipe prettybat];
   };
 }
