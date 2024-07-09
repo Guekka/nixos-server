15a9ed6c5a5df70d476f1cec85109fbebf705a15
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-07-09 19:19:22 +0100

    all: disable message after nixos rebuild. Superseeded by nh

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -1,7 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{pkgs, ...}: {
+{
   imports = [
     ./android.nix
     ./backup.nix
@@ -32,14 +32,6 @@
   # fix hanging at rebuild and wait-online service failing
   # See nixpkgs#180175
   systemd.services.NetworkManager-wait-online.enable = false;
-
-  # Display changes after rebuild, see https://discourse.nixos.org/t/how-to-make-nixos-rebuild-output-more-informative/25549/8
-  system.activationScripts.diff = ''
-    if [[ -e /run/current-system ]]; then
-      echo "NixOS system closure diff:"
-      ${pkgs.nix}/bin/nix store diff-closures /run/current-system "$systemConfig"
-    fi
-  '';
 
   sops.secrets.shared-borgbackup-passphrase.sopsFile = ../secrets.yaml;
 
