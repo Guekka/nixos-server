414ab9aedcca243ba3c36bdd0bfc91f9b414d93a
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-07-12 10:54:44 +0100

    fzf: fix rfv fish syntax

--- home/edgar/global/fzf.nix
+++ home/edgar/global/fzf.nix
@@ -18,26 +18,19 @@
   };
 
   # from <https://junegunn.github.io/fzf/tips/ripgrep-integration/>
-  programs.fish.shellAliases = {
-    rfv = let
-      reload = "reload:rg --column --color=always --smart-case {q} || :";
-      opener = ''
-        if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
-          vim {1} +{2}     # No selection. Open the current line in Vim.
-        else
-          vim +cw -q {+f}  # Build quickfix list for the selected items.
-        fi
-      '';
-    in ''
-      fzf --disabled --ansi --multi \
-          --bind "start:${reload}" --bind "change:${reload}" \
-          --bind "enter:become:${opener}" \
-          --bind "ctrl-o:execute:${opener}" \
-          --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
-          --delimiter : \
-          --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
-          --preview-window '~4,+{2}+4/3,<80(up)' \
-          --query "$argv"
-    '';
-  };
+  programs.fish.functions.rfv.body = ''
+    set reload 'reload:rg --column --color=always --smart-case {q} || :'
+    set opener '
+      $EDITOR {1} +{2}     # No selection. Open the current line in the editor.
+    '
+    fzf --disabled --ansi --multi \
+        --bind "start:$reload" --bind "change:$reload" \
+        --bind "enter:execute($opener)" \
+        --bind "ctrl-o:execute($opener)" \
+        --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
+        --delimiter : \
+        --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
+        --preview-window '~4,+{2}+4/3,<80(up)' \
+        --query "$argv"
+  '';
 }
