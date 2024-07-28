03060f3056ddc5a5bc5ab1e8d560d99fb0cd27e7
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-07-28 19:48:40 +0200

    immich: update to 1.110.0

--- hosts/horus/immich.nix
+++ hosts/horus/immich.nix
@@ -6,7 +6,7 @@
   # The original photos are accessible at /shared/edgar/immich/library
   immichLibrary = "/shared/edgar/immich/library";
   immichAppdataRoot = "${immichRoot}/appdata";
-  immichVersion = "v1.109.2";
+  immichVersion = "v1.110.0";
 
   postgresRoot = "${immichAppdataRoot}/pgsql";
   postgresUser = "immich";
