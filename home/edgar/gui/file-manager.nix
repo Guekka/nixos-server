f927bb9c4b0aebb350d9567418a370414f93dd52
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-11-04 08:37:33 +0100

    file-manager: replace dolphin with nemo

--- home/edgar/gui/file-manager.nix
+++ home/edgar/gui/file-manager.nix
@@ -1,19 +1,5 @@
 {pkgs, ...}: {
   home.packages = [
-    pkgs.libsForQt5.dolphin
-    pkgs.libsForQt5.dolphin-plugins
+    pkgs.stable.cinnamon.nemo-with-extensions
   ];
-
-  xdg.mimeApps = {
-    associations.added = {
-      "x-scheme-handler/file" = "org.kde.dolphin.desktop";
-      "x-directory/normal" = "org.kde.dolphin.desktop";
-    };
-
-    defaultApplications = {
-      "inode/directory" = "org.kde.dolphin.desktop";
-      "x-directory/normal" = "org.kde.dolphin.desktop";
-      "x-scheme-handler/file" = "org.kde.dolphin.desktop";
-    };
-  };
 }
