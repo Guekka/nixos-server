e246de3d18a60fbd32ff36f0fb9b728a74d4ae21
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-12-28 20:09:06 +0100

    staruml: fix overlay not applied

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,32 +4,10 @@
     import ../pkgs {pkgs = final;};
 
   modifications = final: prev: {
-    unstable =
-      import inputs.nixpkgs-unstable {
-        inherit (final) system;
-        config.allowUnfree = true;
-      }
-      // {
-        # some tweaks to staruml
-        staruml = prev.staruml.overrideAttrs (_old: {
-          postFixup = let
-            asar = "${prev.asar}/bin/asar";
-            jq = "${prev.jq}/bin/jq";
-          in ''
-            # extract $out/opt/StarUML/resources/app.asar
-            ${asar} extract $out/opt/StarUML/resources/app.asar $out/tmp
-
-            ${jq} '.config.setappBuild = true' $out/tmp/package.json > $out/tmp/package.json.new
-            mv $out/tmp/package.json.new $out/tmp/package.json
-
-            rm $out/opt/StarUML/resources/app.asar
-            ${asar} pack $out/tmp $out/opt/StarUML/resources/app.asar
-
-            # cleanup
-            rm -rf $out/tmp
-          '';
-        });
-      };
+    unstable = import inputs.nixpkgs-unstable {
+      inherit (final) system;
+      config.allowUnfree = true;
+    };
 
     # required for some games
     steam = prev.steam.override {
@@ -38,6 +16,25 @@
           dotnet-runtime
         ];
     };
+    # some tweaks to staruml
+    staruml = prev.staruml.overrideAttrs (_old: {
+      postFixup = let
+        asar = "${prev.asar}/bin/asar";
+        jq = "${prev.jq}/bin/jq";
+      in ''
+        # extract $out/opt/StarUML/resources/app.asar
+        ${asar} extract $out/opt/StarUML/resources/app.asar $out/tmp
+
+        ${jq} '.config.setappBuild = true' $out/tmp/package.json > $out/tmp/package.json.new
+        mv $out/tmp/package.json.new $out/tmp/package.json
+
+        rm $out/opt/StarUML/resources/app.asar
+        ${asar} pack $out/tmp $out/opt/StarUML/resources/app.asar
+
+        # cleanup
+        rm -rf $out/tmp
+      '';
+    });
 
     helix-latest = inputs.helix.packages.${prev.system}.helix;
   };
