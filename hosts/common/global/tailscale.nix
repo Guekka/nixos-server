3529d8db8d9bdba2fd4460f02bcef4ffe2c8de64
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-12-28 19:54:47 +0100

    tailscale: fix missing auth key file

--- hosts/common/global/tailscale.nix
+++ hosts/common/global/tailscale.nix
@@ -1,8 +1,10 @@
-{
+{config, ...}: {
   services.tailscale = {
     enable = true;
     extraUpFlags = ["--login-server" "https://headscale.ozeliurs.com"];
     useRoutingFeatures = "both";
+
+    authKeyFile = config.sops.secrets.tailscale_key.path;
   };
 
   sops.secrets.tailscale_key.sopsFile = ../secrets.yaml;
