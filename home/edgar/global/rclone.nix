4f3894587047e91cc4fa83196aa7081b59656a54
Edgar B <39066502+Guekka@users.noreply.github.com>
2024-12-28 19:54:39 +0100

    rclone: add pluto

--- home/edgar/global/rclone.nix
+++ home/edgar/global/rclone.nix
@@ -1,30 +1,28 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   home.packages = [pkgs.rclone];
 
   home.sessionVariables = {
     RCLONE_PROGRESS = "1";
   };
 
-  xdg.configFile."rclone/rclone.conf".text = ''
-    [deimos]
-    type = sftp
-    host = deimos
-    shell_type = unix
-    md5sum_command = md5sum
-    sha1sum_command = sha1sum
-
-    [hestia]
-    type = sftp
-    host = hestia
-    shell_type = unix
-    md5sum_command = md5sum
-    sha1sum_command = sha1sum
-
-    [horus]
-    type = sftp
-    host = horus
-    shell_type = unix
-    md5sum_command = md5sum
-    sha1sum_command = sha1sum
-  '';
+  xdg.configFile."rclone/rclone.conf".text = let
+    mkSshHost = host: ''
+      [${host}]
+      type = sftp
+      host = ${host}
+      shell_type = unix
+      md5sum_command = md5sum
+      sha1sum_command = sha1sum
+    '';
+  in
+    lib.concatStringsSep "\n" (map mkSshHost [
+      "horus"
+      "deimos"
+      "pluto"
+      "hestia"
+    ]);
 }
