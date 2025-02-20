84892a578c37ccbd4eb60301d4bd2a159229b6fb
Edgar B <39066502+Guekka@users.noreply.github.com>
2025-02-20 11:07:27 +0100

    activitywatch: actually get aw-sync working

--- home/edgar/optional/activitywatch.nix
+++ home/edgar/optional/activitywatch.nix
@@ -1,6 +1,8 @@
-{pkgs, ...}: {
-  home.sessionVariables.AW_SYNC_DIR = "~/Nextcloud/edgar/activitywatch";
-
+{
+  pkgs,
+  lib,
+  ...
+}: {
   services.activitywatch = {
     enable = true;
     package = pkgs.aw-server-rust;
@@ -9,19 +11,27 @@
       aw-sync.package = pkgs.aw-server-rust;
     };
   };
-
-  # from <https://github.com/jordanisaacs/dotfiles/blob/20d6ff59e1a468b9ce5d78fcf169b31c977bd1b9/modules/users/applications/activitywatch.nix#L4>
-  # awatcher should start and stop depending on WM session target
-  # starting activitywatch should only start awatcher if the WM is active
-  systemd.user.services.activitywatch-watcher-awatcher = let
-    target = "hyprland-session.target";
-  in {
-    Unit = {
-      After = [target];
-      Requisite = [target];
-      PartOf = [target];
+  systemd.user.services = {
+    # from <https://github.com/jordanisaacs/dotfiles/blob/20d6ff59e1a468b9ce5d78fcf169b31c977bd1b9/modules/users/applications/activitywatch.nix#L4>
+    # awatcher should start and stop depending on WM session target
+    # starting activitywatch should only start awatcher if the WM is active
+    activitywatch-watcher-awatcher = let
+      target = "hyprland-session.target";
+    in {
+      Unit = {
+        After = [target];
+        Requisite = [target];
+        PartOf = [target];
+      };
+      Install = {WantedBy = [target];};
     };
-    Install = {WantedBy = [target];};
+
+    # Set sync dir
+    activitywatch-watcher-aw-sync.Service.ExecStart = let
+      exe = lib.getExe' pkgs.aw-server-rust "aw-sync";
+      sync_dir = "/home/edgar/Nextcloud/edgar/activitywatch";
+    in
+      lib.mkForce "${exe} --sync-dir ${sync_dir}";
   };
 
   home.persistence."/persist/backup/home/edgar".directories = [
