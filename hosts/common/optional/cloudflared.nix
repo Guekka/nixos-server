09a2b0fd80902491e6f5731aacd2181640a7cfd3
Edgar B <39066502+Guekka@users.noreply.github.com>
2025-05-23 15:29:46 +0200

    cloudflare: add realIP

--- hosts/common/optional/cloudflared.nix
+++ hosts/common/optional/cloudflared.nix
@@ -1,4 +1,9 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   services.cloudflared = {
     enable = true;
     tunnels = {
@@ -22,4 +27,22 @@
     owner = "cloudflared";
     sopsFile = ../secrets.yaml;
   };
+
+  # Using realIP when behind CloudFlare
+  services.nginx.commonHttpConfig = let
+    realIpsFromList = lib.strings.concatMapStringsSep "\n" (x: "set_real_ip_from  ${x};");
+    fileToList = x: lib.strings.splitString "\n" (builtins.readFile x);
+    cfipv4 = fileToList (pkgs.fetchurl {
+      url = "https://www.cloudflare.com/ips-v4";
+      sha256 = "0ywy9sg7spafi3gm9q5wb59lbiq0swvf0q3iazl0maq1pj1nsb7h";
+    });
+    cfipv6 = fileToList (pkgs.fetchurl {
+      url = "https://www.cloudflare.com/ips-v6";
+      sha256 = "1ad09hijignj6zlqvdjxv7rjj8567z357zfavv201b9vx3ikk7cy";
+    });
+  in ''
+    ${realIpsFromList cfipv4}
+    ${realIpsFromList cfipv6}
+    real_ip_header CF-Connecting-IP;
+  '';
 }
