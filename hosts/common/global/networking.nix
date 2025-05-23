5a5c672aa96549238e43330381344d59c802497f
Edgar B <39066502+Guekka@users.noreply.github.com>
2025-05-23 15:31:24 +0200

    networking: attempt to use networkd

--- hosts/common/global/networking.nix
+++ hosts/common/global/networking.nix
@@ -9,7 +9,7 @@
 
   # Use networkd instead of the pile of shell scripts
   # This appears to break my network setup, so I'm leaving it off for now
-  # networking.useNetworkd = lib.mkDefault true;
+  networking.useNetworkd = lib.mkDefault true;
 
   # The notion of "online" is a broken concept
   # https://github.com/systemd/systemd/blob/e1b45a756f71deac8c1aa9a008bd0dab47f64777/NEWS#L13
