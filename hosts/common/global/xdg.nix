74295961fe4e2774e58267ab29a77e96454b7fde
Edgar B <39066502+Guekka@users.noreply.github.com>
2025-07-12 09:57:27 +0200

    xdg: make environment variables default values
    So that a DE can overwrite them

--- hosts/common/global/xdg.nix
+++ hosts/common/global/xdg.nix
@@ -1,5 +1,5 @@
-{
-  environment.sessionVariables = {
+{lib, ...}: {
+  environment.sessionVariables = lib.mkDefault {
     XDG_CACHE_HOME = "$HOME/.cache";
     XDG_CONFIG_HOME = "$HOME/.config";
     XDG_DATA_DIRS = "/usr/local/share/:/usr/share/";
